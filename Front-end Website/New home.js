47d7cb47fe4be5613588f3a8515be6ed400fe1a1
MOHANRAJ999 <mohanrajcm29@gmail.com>
2024-07-21 14:28:00 +0530

    Add files via upload

---
+++ Front-end Website/New home.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM Loaded! You can add your JavaScript code here.');
+    // Add your JavaScript code here for interactivity
+});
